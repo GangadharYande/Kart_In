dac3c06eae3cf308029e186db8505c0596111f90
Boii <yande.gangadhar@gmail.com>
2024-09-01 20:40:33 +0530

    added model and relationships

---
+++ src/main/java/com/boii_projects/KartIn/KartInApplication.java
@@ -0,0 +1,14 @@
+package com.boii_projects.KartIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartInApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KartInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boii_projects/KartIn/KartInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boii_projects.KartIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
